9540091e9dbb2d9640ac59ed75494906396e3ee4
miguelcrespo <miguelalejandro.crespo.contractor@bbva.com>
2020-05-16 16:45:40 +0200

    RestTemplate

--- initial/src/main/java/com/example/restservice/config/SharePointConfiguration.java
+++ initial/src/main/java/com/example/restservice/config/SharePointConfiguration.java
@@ -1,23 +1,6 @@
 package com.example.restservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
  
 @Configuration
-public class SharePointConfiguration {
-	
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate
-            = new RestTemplate(
-            new BufferingClientHttpRequestFactory(
-                new SimpleClientHttpRequestFactory()
-            )
-        );
-        return restTemplate;
-    }
-
-}
+public class SharePointConfiguration {}
